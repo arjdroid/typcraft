d85948e72341db8090d021716409a9262507560c
Arjun Singh <57623600+Arjdroid@users.noreply.github.com>
2025-12-25 10:43:14 +0530

    Deploy

--- elm.js
+++ elm.js
@@ -4370,6 +4370,89 @@ function _Browser_load(url)
 		}
 	}));
 }
+
+
+
+var _Bitwise_and = F2(function(a, b)
+{
+	return a & b;
+});
+
+var _Bitwise_or = F2(function(a, b)
+{
+	return a | b;
+});
+
+var _Bitwise_xor = F2(function(a, b)
+{
+	return a ^ b;
+});
+
+function _Bitwise_complement(a)
+{
+	return ~a;
+};
+
+var _Bitwise_shiftLeftBy = F2(function(offset, a)
+{
+	return a << offset;
+});
+
+var _Bitwise_shiftRightBy = F2(function(offset, a)
+{
+	return a >> offset;
+});
+
+var _Bitwise_shiftRightZfBy = F2(function(offset, a)
+{
+	return a >>> offset;
+});
+
+
+
+function _Time_now(millisToPosix)
+{
+	return _Scheduler_binding(function(callback)
+	{
+		callback(_Scheduler_succeed(millisToPosix(Date.now())));
+	});
+}
+
+var _Time_setInterval = F2(function(interval, task)
+{
+	return _Scheduler_binding(function(callback)
+	{
+		var id = setInterval(function() { _Scheduler_rawSpawn(task); }, interval);
+		return function() { clearInterval(id); };
+	});
+});
+
+function _Time_here()
+{
+	return _Scheduler_binding(function(callback)
+	{
+		callback(_Scheduler_succeed(
+			A2($elm$time$Time$customZone, -(new Date().getTimezoneOffset()), _List_Nil)
+		));
+	});
+}
+
+
+function _Time_getZoneName()
+{
+	return _Scheduler_binding(function(callback)
+	{
+		try
+		{
+			var name = $elm$time$Time$Name(Intl.DateTimeFormat().resolvedOptions().timeZone);
+		}
+		catch (e)
+		{
+			var name = $elm$time$Time$Offset(new Date().getTimezoneOffset());
+		}
+		callback(_Scheduler_succeed(name));
+	});
+}
 var $elm$core$Basics$EQ = {$: 'EQ'};
 var $elm$core$Basics$GT = {$: 'GT'};
 var $elm$core$Basics$LT = {$: 'LT'};
@@ -5159,7 +5242,283 @@ var $elm$core$Task$perform = F2(
 				A2($elm$core$Task$map, toMessage, task)));
 	});
 var $elm$browser$Browser$element = _Browser_element;
-var $author$project$Main$goalExpression = 'F_g = (G m_1 m_2) / (r^2)';
+var $author$project$Main$EquationSelected = function (a) {
+	return {$: 'EquationSelected', a: a};
+};
+var $elm$core$Array$fromListHelp = F3(
+	function (list, nodeList, nodeListSize) {
+		fromListHelp:
+		while (true) {
+			var _v0 = A2($elm$core$Elm$JsArray$initializeFromList, $elm$core$Array$branchFactor, list);
+			var jsArray = _v0.a;
+			var remainingItems = _v0.b;
+			if (_Utils_cmp(
+				$elm$core$Elm$JsArray$length(jsArray),
+				$elm$core$Array$branchFactor) < 0) {
+				return A2(
+					$elm$core$Array$builderToArray,
+					true,
+					{nodeList: nodeList, nodeListSize: nodeListSize, tail: jsArray});
+			} else {
+				var $temp$list = remainingItems,
+					$temp$nodeList = A2(
+					$elm$core$List$cons,
+					$elm$core$Array$Leaf(jsArray),
+					nodeList),
+					$temp$nodeListSize = nodeListSize + 1;
+				list = $temp$list;
+				nodeList = $temp$nodeList;
+				nodeListSize = $temp$nodeListSize;
+				continue fromListHelp;
+			}
+		}
+	});
+var $elm$core$Array$fromList = function (list) {
+	if (!list.b) {
+		return $elm$core$Array$empty;
+	} else {
+		return A3($elm$core$Array$fromListHelp, list, _List_Nil, 0);
+	}
+};
+var $author$project$Main$equations = $elm$core$Array$fromList(
+	_List_fromArray(
+		[
+			{name: 'Newton\'s Gravitation', typst: 'F_g = (G m_1 m_2) / (r^2)'},
+			{name: 'Euler\'s Identity', typst: 'e^(i pi) + 1 = 0'},
+			{name: 'Pythagorean Theorem', typst: 'a^2 + b^2 = c^2'},
+			{name: 'Quadratic Formula', typst: 'x = (-b plus.minus sqrt(b^2 - 4 a c)) / (2 a)'},
+			{name: 'Einstein\'s Mass-Energy', typst: 'E = m c^2'},
+			{name: 'Schrödinger Equation', typst: 'i hbar (diff Psi) / (diff t) = hat(H) Psi'},
+			{name: 'Maxwell\'s Equations (Gauss)', typst: 'nabla dot bold(E) = rho / epsilon_0'},
+			{name: 'Euler\'s Formula', typst: 'e^(i theta) = cos theta + i sin theta'},
+			{name: 'Binomial Theorem', typst: '(x + y)^n = sum_(k=0)^n binom(n, k) x^(n-k) y^k'},
+			{name: 'Fourier Transform', typst: 'hat(f)(xi) = integral_(-oo)^oo f(x) e^(-2 pi i x xi) dif x'}
+		]));
+var $elm$random$Random$Generate = function (a) {
+	return {$: 'Generate', a: a};
+};
+var $elm$random$Random$Seed = F2(
+	function (a, b) {
+		return {$: 'Seed', a: a, b: b};
+	});
+var $elm$core$Bitwise$shiftRightZfBy = _Bitwise_shiftRightZfBy;
+var $elm$random$Random$next = function (_v0) {
+	var state0 = _v0.a;
+	var incr = _v0.b;
+	return A2($elm$random$Random$Seed, ((state0 * 1664525) + incr) >>> 0, incr);
+};
+var $elm$random$Random$initialSeed = function (x) {
+	var _v0 = $elm$random$Random$next(
+		A2($elm$random$Random$Seed, 0, 1013904223));
+	var state1 = _v0.a;
+	var incr = _v0.b;
+	var state2 = (state1 + x) >>> 0;
+	return $elm$random$Random$next(
+		A2($elm$random$Random$Seed, state2, incr));
+};
+var $elm$time$Time$Name = function (a) {
+	return {$: 'Name', a: a};
+};
+var $elm$time$Time$Offset = function (a) {
+	return {$: 'Offset', a: a};
+};
+var $elm$time$Time$Zone = F2(
+	function (a, b) {
+		return {$: 'Zone', a: a, b: b};
+	});
+var $elm$time$Time$customZone = $elm$time$Time$Zone;
+var $elm$time$Time$Posix = function (a) {
+	return {$: 'Posix', a: a};
+};
+var $elm$time$Time$millisToPosix = $elm$time$Time$Posix;
+var $elm$time$Time$now = _Time_now($elm$time$Time$millisToPosix);
+var $elm$time$Time$posixToMillis = function (_v0) {
+	var millis = _v0.a;
+	return millis;
+};
+var $elm$random$Random$init = A2(
+	$elm$core$Task$andThen,
+	function (time) {
+		return $elm$core$Task$succeed(
+			$elm$random$Random$initialSeed(
+				$elm$time$Time$posixToMillis(time)));
+	},
+	$elm$time$Time$now);
+var $elm$random$Random$step = F2(
+	function (_v0, seed) {
+		var generator = _v0.a;
+		return generator(seed);
+	});
+var $elm$random$Random$onEffects = F3(
+	function (router, commands, seed) {
+		if (!commands.b) {
+			return $elm$core$Task$succeed(seed);
+		} else {
+			var generator = commands.a.a;
+			var rest = commands.b;
+			var _v1 = A2($elm$random$Random$step, generator, seed);
+			var value = _v1.a;
+			var newSeed = _v1.b;
+			return A2(
+				$elm$core$Task$andThen,
+				function (_v2) {
+					return A3($elm$random$Random$onEffects, router, rest, newSeed);
+				},
+				A2($elm$core$Platform$sendToApp, router, value));
+		}
+	});
+var $elm$random$Random$onSelfMsg = F3(
+	function (_v0, _v1, seed) {
+		return $elm$core$Task$succeed(seed);
+	});
+var $elm$random$Random$Generator = function (a) {
+	return {$: 'Generator', a: a};
+};
+var $elm$random$Random$map = F2(
+	function (func, _v0) {
+		var genA = _v0.a;
+		return $elm$random$Random$Generator(
+			function (seed0) {
+				var _v1 = genA(seed0);
+				var a = _v1.a;
+				var seed1 = _v1.b;
+				return _Utils_Tuple2(
+					func(a),
+					seed1);
+			});
+	});
+var $elm$random$Random$cmdMap = F2(
+	function (func, _v0) {
+		var generator = _v0.a;
+		return $elm$random$Random$Generate(
+			A2($elm$random$Random$map, func, generator));
+	});
+_Platform_effectManagers['Random'] = _Platform_createManager($elm$random$Random$init, $elm$random$Random$onEffects, $elm$random$Random$onSelfMsg, $elm$random$Random$cmdMap);
+var $elm$random$Random$command = _Platform_leaf('Random');
+var $elm$random$Random$generate = F2(
+	function (tagger, generator) {
+		return $elm$random$Random$command(
+			$elm$random$Random$Generate(
+				A2($elm$random$Random$map, tagger, generator)));
+	});
+var $elm$core$Bitwise$and = _Bitwise_and;
+var $elm$core$Basics$negate = function (n) {
+	return -n;
+};
+var $elm$core$Bitwise$xor = _Bitwise_xor;
+var $elm$random$Random$peel = function (_v0) {
+	var state = _v0.a;
+	var word = (state ^ (state >>> ((state >>> 28) + 4))) * 277803737;
+	return ((word >>> 22) ^ word) >>> 0;
+};
+var $elm$random$Random$int = F2(
+	function (a, b) {
+		return $elm$random$Random$Generator(
+			function (seed0) {
+				var _v0 = (_Utils_cmp(a, b) < 0) ? _Utils_Tuple2(a, b) : _Utils_Tuple2(b, a);
+				var lo = _v0.a;
+				var hi = _v0.b;
+				var range = (hi - lo) + 1;
+				if (!((range - 1) & range)) {
+					return _Utils_Tuple2(
+						(((range - 1) & $elm$random$Random$peel(seed0)) >>> 0) + lo,
+						$elm$random$Random$next(seed0));
+				} else {
+					var threshhold = (((-range) >>> 0) % range) >>> 0;
+					var accountForBias = function (seed) {
+						accountForBias:
+						while (true) {
+							var x = $elm$random$Random$peel(seed);
+							var seedN = $elm$random$Random$next(seed);
+							if (_Utils_cmp(x, threshhold) < 0) {
+								var $temp$seed = seedN;
+								seed = $temp$seed;
+								continue accountForBias;
+							} else {
+								return _Utils_Tuple2((x % range) + lo, seedN);
+							}
+						}
+					};
+					return accountForBias(seed0);
+				}
+			});
+	});
+var $elm$core$Array$length = function (_v0) {
+	var len = _v0.a;
+	return len;
+};
+var $author$project$Main$init = function (_v0) {
+	return _Utils_Tuple2(
+		{
+			expression: '',
+			goalEquation: {name: '', typst: ''},
+			goalSvg: '',
+			userSvg: ''
+		},
+		A2(
+			$elm$random$Random$generate,
+			$author$project$Main$EquationSelected,
+			A2(
+				$elm$random$Random$int,
+				0,
+				$elm$core$Array$length($author$project$Main$equations) - 1)));
+};
+var $author$project$Main$SvgRendered = function (a) {
+	return {$: 'SvgRendered', a: a};
+};
+var $elm$json$Json$Decode$value = _Json_decodeValue;
+var $author$project$Ports$mathRendered = _Platform_incomingPort('mathRendered', $elm$json$Json$Decode$value);
+var $author$project$Main$subscriptions = function (_v0) {
+	return $author$project$Ports$mathRendered($author$project$Main$SvgRendered);
+};
+var $elm$json$Json$Decode$decodeValue = _Json_run;
+var $elm$json$Json$Decode$field = _Json_decodeField;
+var $elm$core$Array$bitMask = 4294967295 >>> (32 - $elm$core$Array$shiftStep);
+var $elm$core$Basics$ge = _Utils_ge;
+var $elm$core$Elm$JsArray$unsafeGet = _JsArray_unsafeGet;
+var $elm$core$Array$getHelp = F3(
+	function (shift, index, tree) {
+		getHelp:
+		while (true) {
+			var pos = $elm$core$Array$bitMask & (index >>> shift);
+			var _v0 = A2($elm$core$Elm$JsArray$unsafeGet, pos, tree);
+			if (_v0.$ === 'SubTree') {
+				var subTree = _v0.a;
+				var $temp$shift = shift - $elm$core$Array$shiftStep,
+					$temp$index = index,
+					$temp$tree = subTree;
+				shift = $temp$shift;
+				index = $temp$index;
+				tree = $temp$tree;
+				continue getHelp;
+			} else {
+				var values = _v0.a;
+				return A2($elm$core$Elm$JsArray$unsafeGet, $elm$core$Array$bitMask & index, values);
+			}
+		}
+	});
+var $elm$core$Bitwise$shiftLeftBy = _Bitwise_shiftLeftBy;
+var $elm$core$Array$tailIndex = function (len) {
+	return (len >>> 5) << 5;
+};
+var $elm$core$Array$get = F2(
+	function (index, _v0) {
+		var len = _v0.a;
+		var startShift = _v0.b;
+		var tree = _v0.c;
+		var tail = _v0.d;
+		return ((index < 0) || (_Utils_cmp(index, len) > -1)) ? $elm$core$Maybe$Nothing : ((_Utils_cmp(
+			index,
+			$elm$core$Array$tailIndex(len)) > -1) ? $elm$core$Maybe$Just(
+			A2($elm$core$Elm$JsArray$unsafeGet, $elm$core$Array$bitMask & index, tail)) : $elm$core$Maybe$Just(
+			A3($elm$core$Array$getHelp, startShift, index, tree)));
+	});
+var $elm$core$Platform$Cmd$batch = _Platform_batch;
+var $elm$core$Platform$Cmd$none = $elm$core$Platform$Cmd$batch(_List_Nil);
+var $elm$core$Tuple$pair = F2(
+	function (a, b) {
+		return _Utils_Tuple2(a, b);
+	});
 var $elm$json$Json$Encode$object = function (pairs) {
 	return _Json_wrap(
 		A3(
@@ -5190,75 +5549,68 @@ var $author$project$Ports$renderMath = function (_v0) {
 					$elm$json$Json$Encode$string(target))
 				])));
 };
-var $author$project$Main$init = function (_v0) {
-	return _Utils_Tuple2(
-		{expression: '', goalExpression: $author$project$Main$goalExpression, goalSvg: '', userSvg: ''},
-		$author$project$Ports$renderMath(
-			{expression: $author$project$Main$goalExpression, target: 'goal'}));
-};
-var $author$project$Main$SvgRendered = function (a) {
-	return {$: 'SvgRendered', a: a};
-};
-var $elm$json$Json$Decode$value = _Json_decodeValue;
-var $author$project$Ports$mathRendered = _Platform_incomingPort('mathRendered', $elm$json$Json$Decode$value);
-var $author$project$Main$subscriptions = function (_v0) {
-	return $author$project$Ports$mathRendered($author$project$Main$SvgRendered);
-};
-var $elm$json$Json$Decode$decodeValue = _Json_run;
-var $elm$json$Json$Decode$field = _Json_decodeField;
-var $elm$core$Platform$Cmd$batch = _Platform_batch;
-var $elm$core$Platform$Cmd$none = $elm$core$Platform$Cmd$batch(_List_Nil);
-var $elm$core$Tuple$pair = F2(
-	function (a, b) {
-		return _Utils_Tuple2(a, b);
-	});
 var $elm$json$Json$Decode$string = _Json_decodeString;
 var $author$project$Main$update = F2(
 	function (msg, model) {
-		if (msg.$ === 'ExpressionChanged') {
-			var newExpr = msg.a;
-			return _Utils_Tuple2(
-				_Utils_update(
-					model,
-					{expression: newExpr}),
-				$author$project$Ports$renderMath(
-					{expression: newExpr, target: 'user'}));
-		} else {
-			var jsonValue = msg.a;
-			var decoder = A3(
-				$elm$json$Json$Decode$map2,
-				$elm$core$Tuple$pair,
-				A2($elm$json$Json$Decode$field, 'target', $elm$json$Json$Decode$string),
-				A2($elm$json$Json$Decode$field, 'svg', $elm$json$Json$Decode$string));
-			var _v1 = A2($elm$json$Json$Decode$decodeValue, decoder, jsonValue);
-			_v1$2:
-			while (true) {
-				if (_v1.$ === 'Ok') {
-					switch (_v1.a.a) {
-						case 'user':
-							var _v2 = _v1.a;
-							var svg = _v2.b;
-							return _Utils_Tuple2(
-								_Utils_update(
-									model,
-									{userSvg: svg}),
-								$elm$core$Platform$Cmd$none);
-						case 'goal':
-							var _v3 = _v1.a;
-							var svg = _v3.b;
-							return _Utils_Tuple2(
-								_Utils_update(
-									model,
-									{goalSvg: svg}),
-								$elm$core$Platform$Cmd$none);
-						default:
-							break _v1$2;
+		switch (msg.$) {
+			case 'ExpressionChanged':
+				var newExpr = msg.a;
+				return _Utils_Tuple2(
+					_Utils_update(
+						model,
+						{expression: newExpr}),
+					$author$project$Ports$renderMath(
+						{expression: newExpr, target: 'user'}));
+			case 'SvgRendered':
+				var jsonValue = msg.a;
+				var decoder = A3(
+					$elm$json$Json$Decode$map2,
+					$elm$core$Tuple$pair,
+					A2($elm$json$Json$Decode$field, 'target', $elm$json$Json$Decode$string),
+					A2($elm$json$Json$Decode$field, 'svg', $elm$json$Json$Decode$string));
+				var _v1 = A2($elm$json$Json$Decode$decodeValue, decoder, jsonValue);
+				_v1$2:
+				while (true) {
+					if (_v1.$ === 'Ok') {
+						switch (_v1.a.a) {
+							case 'user':
+								var _v2 = _v1.a;
+								var svg = _v2.b;
+								return _Utils_Tuple2(
+									_Utils_update(
+										model,
+										{userSvg: svg}),
+									$elm$core$Platform$Cmd$none);
+							case 'goal':
+								var _v3 = _v1.a;
+								var svg = _v3.b;
+								return _Utils_Tuple2(
+									_Utils_update(
+										model,
+										{goalSvg: svg}),
+									$elm$core$Platform$Cmd$none);
+							default:
+								break _v1$2;
+						}
+					} else {
+						break _v1$2;
 					}
-				} else {
-					break _v1$2;
 				}
-			}
-			return _Utils_Tuple2(model, $elm$core$Platform$Cmd$none);
+				return _Utils_Tuple2(model, $elm$core$Platform$Cmd$none);
+			default:
+				var index = msg.a;
+				var _v4 = A2($elm$core$Array$get, index, $author$project$Main$equations);
+				if (_v4.$ === 'Just') {
+					var equation = _v4.a;
+					return _Utils_Tuple2(
+						_Utils_update(
+							model,
+							{goalEquation: equation}),
+						$author$project$Ports$renderMath(
+							{expression: equation.typst, target: 'goal'}));
+				} else {
+					return _Utils_Tuple2(model, $elm$core$Platform$Cmd$none);
+				}
 		}
 	});
 var $author$project$Main$ExpressionChanged = function (a) {
@@ -5421,7 +5773,7 @@ var $author$project$Main$view = function (model) {
 							]),
 						_List_fromArray(
 							[
-								$elm$html$Html$text('Goal:')
+								$elm$html$Html$text('Goal: ' + model.goalEquation.name)
 							])),
 						A2(
 						$elm$html$Html$div,
